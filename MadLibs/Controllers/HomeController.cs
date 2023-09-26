69a33ae1ef36e646e04345318dcc7318f4321d04
object-ions <moshikoatia@gmail.com>
2023-09-25 17:10:49 -0700

    add extanded version of madlibs with different form options.
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,26 +5,37 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    // [Route("/")]
-    // public ActionResult Letter()
-    // {
-    // MadLibs myMadLibs = new MadLibs();
-    //   myMadLibs.Adjective = "Lina";
-    //   myMadLibs.Food = "Lina";
-    //   myMadLibs.Verb = "Lina";
-
-    //   return View(myMadLibs);
-    // }
+    [HttpGet]
     [Route("/")]
-    public ActionResult Form() { return View(); }
-    [Route("/story")]
-    public ActionResult Story(string adjective, string food, string verb)
+    public ActionResult Form()
     {
-      Inputs myInputs = new Inputs();
-      myInputs.Adjective = adjective;
-      myInputs.Food = food;
-      myInputs.Verb = verb;
-      return View(myInputs);
+      return View();
+    }
+
+    [HttpPost]
+    [Route("/story")]
+    public ActionResult Story(string adjective, string food, string verb, string formId)
+    {
+      Inputs myInputs = new Inputs
+      {
+        Adjective = adjective,
+        Food = food,
+        Verb = verb
+      };
+
+      switch (formId)
+      {
+        case "taco":
+          return View("story-taco", myInputs); // Use if you have a View named "story-taco.cshtml"
+        case "gingerbread":
+          return View("story-gingerbread", myInputs); // Use if you have a View named "story-gingerbread.cshtml"
+        case "photoshoot":
+          return View("story-photoshoot", myInputs); // Use if you have a View named "story-photoshoot.cshtml"
+        case "pizza-party":
+          return View("story-pizza-party", myInputs); // Use if you have a View named "story-pizza-party.cshtml"
+        default:
+          return NotFound(); // Return a 404 not found if the formId doesn't match any known cases
+      }
     }
   }
 }
